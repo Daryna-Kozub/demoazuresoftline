1697f44e178d384395b17bb4d111c0a63a7a820f
Daryna-Kozub <51606604+Daryna-Kozub@users.noreply.github.com>
2020-05-05 14:51:41 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,5 @@
-variable "resourcename" {
-  default = "webRG"
-  description = "Name for resource group to create VM in"
-}
-
 variable "prefix" {
     default = "web"
 }
 
-variable "vm_name" {
-  default = "webVmM"
-  description = "Name for VM to be created"
-}
 
-variable "default_environment_tag" {
-  default = "web"
-  description = "Default environment tag for the resources of stack"
-}
